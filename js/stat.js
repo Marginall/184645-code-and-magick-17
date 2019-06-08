3fceebfa85817efc9fbeb1a9ab4903099da15cbb
Marginall <mistmee2016@gmail.com>
2019-06-08 09:38:08 +0300

    mod2 task1

--- js/stat.js
+++ js/stat.js
@@ -2,18 +2,20 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
-var CLOUD_Y = 20;
+var CLOUD_Y = 10;
 var CLOUD_GAP = 10;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
-var BAR_X = 120;
-var BAR_Y = 40;
+var TEXT_X = 120;
+var TEXT_Y = 40;
 var FONT = 'bold 16px PT Mono';
 var FONT_GAP = 20;
+var mainColor = '#fff';
+var darkColor = 'rgba(0, 0, 0, 0.7)';
 var userBarColor = 'rgba(255, 0, 0, 1)';
-var innerUserBarColor = 'blue';
-var verticalGap = FONT_GAP / 2;
+var positionForTotalX = TEXT_X + FONT_GAP;
+var positionForTotalY = TEXT_Y + FONT_GAP + FONT_GAP;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -23,13 +25,14 @@ var renderCloud = function (ctx, x, y, color) {
 var renderText = function (ctx, text, x, y, font) {
   ctx.fillText(text, x, y);
   ctx.font = font;
+  ctx.fillStyle = darkColor;
 };
 
 var renderPreview = function (ctx) {
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-  renderCloud(ctx, CLOUD_X - CLOUD_GAP, CLOUD_Y - CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
-  renderText(ctx, 'Ура вы победили!', BAR_X, BAR_Y, FONT);
-  renderText(ctx, 'Список результатов:', BAR_X, BAR_Y + FONT_GAP, FONT);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, mainColor);
+  renderCloud(ctx, CLOUD_X - CLOUD_GAP, CLOUD_Y - CLOUD_GAP, darkColor);
+  renderText(ctx, 'Ура вы победили!', TEXT_X, TEXT_Y, FONT);
+  renderText(ctx, 'Список результатов:', TEXT_X, TEXT_Y + FONT_GAP, FONT);
 };
 
 var getMaxElement = function (arr) {
@@ -47,15 +50,20 @@ var getMaxElement = function (arr) {
 window.renderStatistics = function (ctx, names, times) {
   renderPreview(ctx);
   var maxTime = getMaxElement(times);
-  var startNamePositionX = BAR_X + FONT_GAP;
-  var startNamePositionY = CLOUD_HEIGHT - verticalGap;
-  console.log(startNamePositionY);
 
   for (var i = 0; i < names.length; i++) {
-    renderText(ctx, names[i], startNamePositionX, startNamePositionY, FONT);
-    ctx.fillRect(BAR_X + FONT_GAP, CLOUD_HEIGHT - FONT_GAP - verticalGap - BAR_HEIGHT, BAR_WIDTH, BAR_HEIGHT);
-    renderText(ctx, Math.floor(times[i]), startNamePositionX, CLOUD_HEIGHT - FONT_GAP - verticalGap - BAR_HEIGHT - verticalGap, FONT);
-    startNamePositionX = startNamePositionX + BAR_WIDTH + BAR_GAP;
-  }
+    ctx.fillStyle = darkColor;
+    renderText(ctx, names[i], positionForTotalX, CLOUD_HEIGHT - FONT_GAP, FONT);
+    renderText(ctx, Math.floor(times[i]), positionForTotalX, positionForTotalY + (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime), FONT);
 
+    if (i === 0) {
+      ctx.fillStyle = userBarColor;
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + parseFloat(Math.random()) + ')';
+    }
+
+    ctx.fillRect(positionForTotalX, (positionForTotalY + FONT_GAP) + (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime), BAR_WIDTH, ((BAR_HEIGHT * times[i]) / maxTime) - FONT_GAP);
+
+    positionForTotalX = positionForTotalX + BAR_WIDTH + BAR_GAP;
+  }
 };
